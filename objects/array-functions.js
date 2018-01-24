9cbeedd978c98feff0733775f4dd3fa0cb54d05e
Artem <artshon@astp.cn.ua>
2018-01-24 12:59:52 +0200

    Кривой аналог функции reverse() Версия 1.0

--- objects/array-functions.js
+++ objects/array-functions.js
@@ -12,8 +12,34 @@ function reverseArray(arr) {
     console.log(arrReverseClone);
 }
 
-reverseArray(array);
 
-//test
+//Вторая работает как оригинальный метод reverse – она меняет порядок элементов на обратный в том массиве,
+// который был ей передан в качестве аргумента. Не используйте стандартный метод reverse.
+var arrayOne = [1, 2, 3, 4, 5];
 
 
+function reverseArrayInPlace(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        arr.unshift(arr[arr.length - i]);
+        arr.pop();
+        console.log(arr);
+    }
+    return arr;
+}
+
+//console.log(reverseArrayInPlace(arrayOne));
+
+function reverseArrayInPlaceA(arr) {
+    var i = 1;
+    do {
+        var b = arr[arr.length - i];
+        arr.unshift(b);
+        arr.pop();
+        i++;
+        console.log(arr);
+    } while (i < arr.length);
+
+    return arr;
+}
+
+reverseArrayInPlaceA(arrayOne);
